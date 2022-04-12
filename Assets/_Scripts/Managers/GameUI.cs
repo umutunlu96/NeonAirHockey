b2252e6261e5269deb010d368742eacafd9d48d5
umutunlu96 <umutt.unlu.96@gmail.com>
2022-04-13 02:30:45 +0300

    lvl 51-2

--- Assets/_Scripts/Managers/GameUI.cs
+++ Assets/_Scripts/Managers/GameUI.cs
@@ -87,22 +87,18 @@ public class GameUI : MonoBehaviour
         if (gameManager.timer >= gameManager.timer * 2 / 3)
         {
             //goodJobText.text = "FANTASTIC!";
-            StartCoroutine(Stars(3));
         }
         else if (gameManager.timer < timer * 2 / 3 && gameManager.timer >= timer / 3)
         {
             //goodJobText.text = "AWESOME!";
-            StartCoroutine(Stars(2));
         }
         else if (gameManager.timer <= timer * 1 / 3)
         {
             //goodJobText.text = "WELL DONE!";
-            StartCoroutine(Stars(1));
         }
         else
         {
             //goodJobText.text = "GOOD!";
-            StartCoroutine(Stars(0));
         }
     }
 
@@ -142,40 +138,5 @@ public class GameUI : MonoBehaviour
     {
         EffectManager.instance.VibrationToggle();
         vibrateOff.gameObject.SetActive(EffectManager.instance.isNotVibrating);
-    }
-
-    private IEnumerator Stars(int shineNumber)
-    {
-        yield return new WaitForSeconds(.5f);
-
-        switch (shineNumber)
-        {
-            case 3:
-                yield return new WaitForSeconds(.15f);
-                star1.sprite = shineStar;
-                yield return new WaitForSeconds(.15f);
-                star2.sprite = shineStar;
-                yield return new WaitForSeconds(.15f);
-                star3.sprite = shineStar;
-                break;
-            case 2:
-                yield return new WaitForSeconds(.15f);
-                star1.sprite = shineStar;
-                yield return new WaitForSeconds(.15f);
-                star2.sprite = shineStar;
-                star3.sprite = darkStar;
-                break;
-            case 1:
-                yield return new WaitForSeconds(.15f);
-                star1.sprite = shineStar;
-                star2.sprite = darkStar;
-                star3.sprite = darkStar;
-                break;
-            case 0:
-                star1.sprite = darkStar;
-                star2.sprite = darkStar;
-                star3.sprite = darkStar;
-                break;
-        }
     }
 }
